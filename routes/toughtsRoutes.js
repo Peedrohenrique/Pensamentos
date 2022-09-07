1a6119bf115e495190bb7320d130cd6d1434aa4c
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2022-09-07 20:01:41 -0300

    Finalizando autenticação e etc..
    e tbm Middleware de verificação de autenticação

--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const ToughController = require("../controllers/ToughtController");
 
+// helpers
+const checkAuth = require("../helpers/auth").checkAuth;
+
+router.get("/dashboard", checkAuth, ToughController.dashboard);
 router.get("/", ToughController.showToughts);
 
 module.exports = router;
